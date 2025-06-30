fa73624b2ca2a23719d63d40235c4d9c42f3ed5c
Faristan Falih Athalla <faristanfalihathalla@gmail.com>
2025-06-30 20:39:49 +0700

    testing

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:login_ui/loginpage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+      ),
+      home : Loginpage(),
+    );
+  }
+}
